cba82da06599b47c20627f2aeeb4bae343b2aed6
Rene Rivera <grafikrobot@gmail.com>
2021-06-08 22:09:04 -0500

    Upload hash_predef/1.14-alpha revision a08d7e1a807a121d787bed0cfc505c1a.

---
+++ hash_predef/1.14-alpha/a08d7e1a807a121d787bed0cfc505c1a/files/conanfile.py
@@ -0,0 +1,42 @@
+'''
+@author: René Ferdinand Rivera Morell
+@copyright: Copyright René Ferdinand Rivera Morell 2021
+@license:
+    Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+'''
+from conans import ConanFile, tools, errors
+import os
+
+
+required_conan_version = ">=1.32.0"
+
+
+class HashPredefConan(ConanFile):
+    name = "hash_predef"
+    homepage = "https://github.com/grafikrobot/hash-predef"
+    description = '''\
+This library defines a set of compiler, architecture, operating system,
+library, and other version numbers from the information it can gather of
+C, C++, Objective C, and Objective C++ predefined macros or those defined in
+generally available headers.
+
+This is an entirely standlone version of the Boost.Predef library.
+'''
+    topics = ("header", 'header-only', 'preprocessor',
+              "#ifdef", "cross-platform")
+    license = "BSL-1.0"
+    no_copy_source = True
+    url = "https://github.com/grafikrobot/hash-predef"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version],
+                  strip_root=True, destination="source_subfolder")
+
+    def package(self):
+        self.copy(pattern="*.h", dst="include",
+                  src=os.path.join("source_subfolder", "include"))
+
+    def package_id(self):
+        self.info.header_only()
