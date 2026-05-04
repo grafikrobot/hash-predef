28813a0215e47c905543e7b976966e34694a8931
Rene Rivera <grafikrobot@gmail.com>
2026-05-04 18:04:47 -0500

    Update AZP compilers.

--- tools/ci/linux-cxx-install.sh
+++ tools/ci/linux-cxx-install.sh
@@ -12,6 +12,7 @@ OS_ISSUE=`cat /etc/issue | head -n1 | cut -d " " -f1`
 echo ">>>>> OS Issue: ${OS_ISSUE}"
 
 PACKAGES=${PACKAGES:-build-essential}
+# APT_OPT="-o Acquire::Retries=3 -yq --no-install-suggests --no-install-recommends"
 APT_OPT="-o Acquire::Retries=3 -yq"
 
 set -e
@@ -27,9 +28,12 @@ echo ">>>>>"
 echo ">>>>> APT: REPO.."
 echo ">>>>>"
 ${SUDO} apt-get ${APT_OPT} install software-properties-common
+# if test "${OS_ISSUE}" = "Ubuntu" ; then
+#     ${SUDO} apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
+# fi
 if test -n "${LLVM_OS}" ; then
-	wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-	${SUDO} apt-add-repository "deb http://apt.llvm.org/${LLVM_OS}/ llvm-toolchain-${LLVM_OS}-${LLVM_VER} main"
+    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+    ${SUDO} apt-add-repository "deb https://apt.llvm.org/${LLVM_OS}/ llvm-toolchain-${LLVM_OS}-${LLVM_VER} main"
 fi
 echo ">>>>>"
 echo ">>>>> APT: UPDATE 2/2.."
@@ -44,4 +48,4 @@ ${SUDO} apt-get ${APT_OPT} install ${PACKAGES}
 # subject to the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE.txt)
 #
-# Copyright René Ferdinand Rivera Morell 2020-2022.
+# Copyright René Ferdinand Rivera Morell
